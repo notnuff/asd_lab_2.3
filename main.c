93166fa36373c88d0a9bcc03de25c20c4500b1e8
notnuff <yaroshsasha960@gmail.com>
2023-04-19 21:11:22 +0300

    finally, arcs (almost nice)

--- main.c
+++ main.c
@@ -29,19 +29,120 @@ typedef struct screen_space_point_t {
     char number[2];
 } point_t;
 
+typedef struct vector {
+    int start_x;
+    int start_y;
+    int end_x;
+    int end_y;
+} vec_4;
+
+
+typedef struct serv_vector { //it`s relative to start_x, start_y
+    int x;
+    int y;
+} vec_2;
+
 // X routines
 void init_x();
 void close_x();
 void redraw ();
 
-//it`s not allowed to make height >= width / 1.15
+
 int SCREEN_WIDTH = 1000;
 int SCREEN_HEIGHT = 800;
+unsigned long bpen = 0x3200FF;
+unsigned long wpen = 0xFFFFFF;
+
 
 
 // graph drawing routines
-void arrow(double fi, int px, int py);
-void draw_graph(point_t *graph, int size);
+void arrow(double theta, int px, int py);
+double vec_dist (vec_2* vec) {
+    double dist = sqrt(vec->x * vec->x + vec->y * vec->y);
+    return dist;
+}
+double vectors_cos (vec_2* vec1, vec_2* vec2) {
+    double cos = (vec1->x * vec2 ->x + vec1->y * vec2->y) /
+            vec_dist(vec1) * vec_dist(vec2);
+    return cos;
+}
+void draw_graph_vertices (point_t *graph, int size);
+
+//за цю функію взагалі треба 13 балів ставити)
+void draw_arc (vec_4 *vector, int radius) {
+    vec_2 *vec = malloc(sizeof (vec_2));
+    vec_2 *vec_OX = malloc(sizeof (vec_2));
+    *vec_OX = (vec_2) {1,  0};
+    vec->x = vector->end_x - vector->start_x;
+    vec->y = vector->end_y - vector->start_y;
+    int midpoint_x = vec->x / 2;
+    int midpoint_y = vec->y / 2;
+    double cosB = vectors_cos(vec, vec_OX);
+    double sinA = cosB;
+    double cosA = cosB > 0 ? sqrt(1-cosB * cosB) : -sqrt(1-cosB * cosB);
+    int offset_y = sinA * radius;
+    int offset_x = cosA * radius;
+    int center_x = vector->start_x + midpoint_x - offset_x;
+    int center_y = vector->start_y + midpoint_y + offset_y;
+    int rad2 = sqrt((center_x - vector->start_x) * (center_x - vector->start_x) +
+            (center_y - vector->start_y) * (center_y - vector->start_y));
+    vec->x = -(center_x - vector->end_x);
+    vec->y = -(center_y - vector->end_y);
+    cosB = vectors_cos(vec, vec_OX);
+    vec->x = -(center_x - vector->start_x);
+    vec->y = -(center_y - vector->start_y);
+    cosA = vectors_cos(vec, vec_OX);
+
+    int B = 64 * acos(cosB) * 180 / 3.1415;
+    if (cosB > 0) B = -B;
+    int A = 64 * acos(cosA) * 180 / 3.1415;
+    if (cosA > 0) A = -A;
+    printf("\n %i %i \n", A/64, B/64);
+    XDrawLine(dis, win, gc, center_x, center_y, vector->start_x + midpoint_x, vector->start_y + midpoint_y);
+    XDrawArc(dis, win, gc, center_x - rad2, center_y - rad2,
+             2*rad2, 2*rad2, B, abs(B - A));
+}
+void draw_graph_o (point_t *graph, int **relation_matrix, int size) {
+    int point_1 = 0;
+    int point_2 = size / 3;
+    int point_3 = 2 * size / 3;
+    XSetForeground(dis, gc, wpen);
+    for (int i = point_1; i < point_2; i++) {
+        for (int j = 0; j < size; j++) {
+            int x_offset = 2 * (graph[j].x - graph[i].x);
+            int y_offset = 2 * (graph[j].y - graph[i].y);
+
+            if (j < point_2) {
+                switch (abs(i - j)) {
+                    case 0:
+                        if (relation_matrix[i][i]) {
+                            XDrawArc(dis, win, gc, graph[j].x + 5, graph[j].y - 20,
+                                     2 * 15, 2 * 15, 0, 360 * 64);
+                        }
+                        break;
+                    case 1:
+                        if (relation_matrix[i][j]) {
+                            XDrawLine(dis, win, gc, graph[i].x, graph[i].y, graph[j].x, graph[j].y);
+                        }
+                        break;
+                    default:
+                        if (relation_matrix[i][j]) {
+                            vec_4 *this_vec = malloc(sizeof (vec_4));
+                            *this_vec = (vec_4) {graph[i].x, graph[i].y,
+                                                 graph[j].x, graph[j].y};
+
+                            draw_arc (this_vec, 200);
+                        }
+                }
+            }
+            else {
+                if (relation_matrix[i][j]) {
+                    XDrawLine(dis, win, gc, graph[i].x, graph[i].y, graph[j].x, graph[j].y);
+                }
+            }
+        }
+    }
+}
 
 // matrix functions
 double **randm(int size1, int size2);
@@ -63,38 +164,51 @@ int main() {
     service_mat = randm(n, n);
     int **rel_mat;
     rel_mat = mat_create(n, n);
-
-    rel_mat = mulmr(c, service_mat, rel_mat, n, n, 1);
-    print_mat(rel_mat, n, n);
-
     point_t *graph;
-    graph = tri_graph_create(graph, n);
+
     XEvent event;
     KeySym key;
     char text[255]; //keyboard buffer
 
     init_x();//window created
 
-    while(1) {
+    while (1) {
         XNextEvent(dis, &event);
         switch (event.type) {
             case Expose:
                 if (event.xexpose.count == 0) redraw();
                 break;
             case KeyPress:
-                if(XLookupString(&event.xkey, text, 255, &key, 0) == 1) {
-                    if (text[0] == 'q') {
-                        close_x();
+                if (XLookupString(&event.xkey, text, 255, &key, 0) == 1) {
+                    switch (text[0]) {
+                        case 'q':
+                            free_mat(service_mat, n);
+                            free_mat(rel_mat, n);
+                            free(graph);
+                            close_x();
+                            return 0;
+                            break;
+                        case 'o':
+                            redraw();
+                            rel_mat = mulmr(c, service_mat, rel_mat, n, n, 1);
+                            printf("drawing oriented matrix: \n");
+                            print_mat(rel_mat, n, n);
+                            graph = tri_graph_create(graph, n);
+                            draw_graph_o(graph, rel_mat, n);
+                            draw_graph_vertices(graph, n);
+
+                            break;
+                        case 'u':
+
+                            break;
+
                     }
-                    else {
-                        printf("You pressed the %c key!\n", text[0]);
-                        draw_graph(graph, n);
-                    }
+                    break;
                 }
-                break;
         }
     }
 }
+
 
 void init_x() {
     unsigned long background = 0x3D3D3D;
@@ -121,15 +235,16 @@ void close_x() {
 
 void redraw() {
     XClearWindow(dis, win);
+
 }
 
-void arrow (double fi, int px, int py) {
-    fi = 3.1416 * (180.0 - fi) / 180.0;
+void arrow (double theta, int px, int py) {
+    theta = 3.1416 * (180.0 - theta) / 180.0;
     int lx, ly, rx, ry;
-    lx = px + (int) 15 * cos(fi + 0.3);
-    rx = px + (int) 15 * cos(fi - 0.3);
-    ly = py + (int) 15 * sin(fi + 0.3);
-    ry = py + (int) 15 * sin(fi - 0.3);
+    lx = px + (int) 15 * cos(theta + 0.3);
+    rx = px + (int) 15 * cos(theta - 0.3);
+    ly = py + (int) 15 * sin(theta + 0.3);
+    ry = py + (int) 15 * sin(theta - 0.3);
 
     XDrawLine(dis, win, gc, lx, ly, px, py);
     XDrawLine(dis, win, gc, px, py, rx, ry);
@@ -139,15 +254,15 @@ point_t *tri_graph_create(point_t *tar_graph, int graph_size) {
     double as_ratio = (double) SCREEN_HEIGHT / SCREEN_WIDTH;
     tar_graph = malloc(sizeof (point_t) * graph_size);
     serv_point_t *serv_points = malloc(sizeof (serv_point_t) * graph_size);
-    double indent_x = (double) 50 / SCREEN_WIDTH;
-    double indent_y = (double) 50 / SCREEN_HEIGHT;
+    double indent_x = (double) 100 / SCREEN_WIDTH;
+    double indent_y = (double) 400 / SCREEN_HEIGHT;
     int point_1 = 0;
     int point_2 = graph_size / 3;
     int point_3 = 2 * graph_size / 3;
 
-    serv_points[point_1] = (serv_point_t) {0, 1 - indent_y};
-    serv_points[point_2] = (serv_point_t) {1.15 - indent_x, -1 + indent_y};
-    serv_points[point_3] = (serv_point_t) {-1.15 + indent_x, -1 + indent_y};
+    serv_points[point_1] = (serv_point_t) {0, 1 - indent_y / 5};
+    serv_points[point_2] = (serv_point_t) {1 - indent_x, -1 + indent_y};
+    serv_points[point_3] = (serv_point_t) {-1 + indent_x, -1 + indent_y};
 
     //interpolation (I guess?) for non-anchor points of triangle
     for (int i = point_1 + 1, j = 1 ; i < point_2; i++, j++) {
@@ -170,7 +285,7 @@ point_t *tri_graph_create(point_t *tar_graph, int graph_size) {
     }
     //translating into the screen space
     for (int i = 0; i < graph_size; i++) {
-        tar_graph[i].x =  SCREEN_WIDTH - (int) (serv_points[i].x * SCREEN_WIDTH * as_ratio + SCREEN_WIDTH) / 2;
+        tar_graph[i].x =  SCREEN_WIDTH + (int) (serv_points[i].x * SCREEN_WIDTH * as_ratio - SCREEN_WIDTH) / 2;
         tar_graph[i].y =  SCREEN_HEIGHT - (int) (serv_points[i].y * SCREEN_HEIGHT + SCREEN_HEIGHT) / 2 ;
         sprintf(tar_graph[i].number, "%d", i + 1);
         printf("%i %i %s\n", tar_graph[i].x, tar_graph[i].y, tar_graph[i].number);
@@ -179,7 +294,7 @@ point_t *tri_graph_create(point_t *tar_graph, int graph_size) {
     return tar_graph;
 }
 
-void draw_graph(point_t *graph, int size) {
+void draw_graph_vertices(point_t *graph, int size) {
     XSetForeground(dis, gc, 0xFFFFFF);
     int r = 15;
     for (int i = 0; i < size; i++) {
@@ -188,8 +303,6 @@ void draw_graph(point_t *graph, int size) {
                  2*r, 2*r,
                  0, 360 * 64);
     }
-    unsigned long bpen = 0x3200FF;
-    unsigned long kpen = 0xFFFFFF;
 
 //    XSetForeground(dis, gc, kpen);
 //    XDrawLine(dis, win, gc, nx[0], ny[0], nx[1], ny[1]);
@@ -207,9 +320,11 @@ void draw_graph(point_t *graph, int size) {
     for (int i = 0; i < size; i++){
         XDrawArc(dis, win, gc, graph[i].x - r, graph[i].y - r,
                  2*r, 2*r, 0, 360*64);
-        XDrawString(dis, win, gc, graph[i].x - 2, graph[i].y + 4, graph[i].number, 2);
+        XDrawString(dis, win, gc, graph[i].x - 2, graph[i].y + 4,
+                    graph[i].number, i < 9 ? 1 : 2);
     }
 }
+
 
 
 double **randm(int size1, int size2) {
